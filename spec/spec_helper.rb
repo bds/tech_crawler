5158416d94bd039b42b56f6a2ab3200c2698dacb
Brian D. Smith <bd9302@gmail.com>
2015-08-18 10:04:17 -0700

    increases VCR fixtures to 2 weeks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'vcr'
 
 VCR.configure do |config|
   config.default_cassette_options = {
-    record: :new_episodes, re_record_interval: (72 * 3600) # 3 days
+    record: :new_episodes, re_record_interval: (14 * 24 * 3600) # 2 weeks
   }
   config.cassette_library_dir = "fixtures/vcr_cassettes"
   config.hook_into :typhoeus
